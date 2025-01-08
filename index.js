a82429641273007c7737fbc50f0c3275ab9b03a4
shansundar <shansundar345@gmail.com>
2025-01-08 12:35:01 +0530

    getcall

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 const PORT = 4000;
+
+app.use("/get", (req, res) => {
+  res.status(200).json({ message: "get call is success" });
+});
+
 app.listen(PORT, () => {
   console.log("connect === successfully");
 });
